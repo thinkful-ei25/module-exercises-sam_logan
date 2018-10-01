817c87ceb22d0ff8ad7745d0f62069a7a9bb62b8
Sam Dalton <spd4nf@virginia.edu>
2018-10-01 11:10:46 -0500

    updated store to use create,update, and delete items

--- scripts/store.js
+++ scripts/store.js
@@ -1,19 +1,51 @@
 'use strict';
+/* global shoppingList, cuid, Item */
 const store = (function (){
     
-    const items = [
-        { id: cuid(), name: 'apples', checked: false },
-        { id: cuid(), name: 'oranges', checked: false },
-        { id: cuid(), name: 'milk', checked: true },
-        { id: cuid(), name: 'bread', checked: false }
-    ];
-    const hideCheckedItems = false ;
-    const searchTerm = '';
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  const hideCheckedItems = false ;
+  const searchTerm = '';
 
-    return {
-        items,
-        hideCheckedItems,
-        searchTerm
-    };
+  const findByID = function(id){
+    return store.items.find(id);
+  };
+
+  const addItem = function(name){
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(e){
+      console.log(e.message);
+    }
+  };
+
+  const findAndToggleChecked = function(id){
+    this.findByID(id).checked = !this.findByID(id).checked;
+  };
+
+  const findAndUpdateName = function(id, newName){
+    this.findByID(id).name = newName;
+  };
+
+  const findAndDelete = function(id){
+    const itemIndex = this.items.findIndex(id => id === id);
+    store.items.splice(itemIndex,1);
+  };
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm,
+    findByID,
+    addItem,
+    findAndToggleChecked,
+    findAndDelete,
+    findAndUpdateName
+  };
 
 }() );
